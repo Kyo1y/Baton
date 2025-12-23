b569c45818d1e3264f59c9ccc7d3ee9903843606
Kairat Sadyrbekov <kyoly@Kairats-MacBook-Air.local>
2025-12-22 20:43:41 -0500

    lint fix

--- baton_frontend/app/(actions)/finalizeTransfer.ts
+++ baton_frontend/app/(actions)/finalizeTransfer.ts
@@ -2,26 +2,24 @@
 
 import { runTransfer } from "@/lib/transfer/runTransfer";
 import { prisma } from "@/lib/prisma";
-import { TransferStatus } from "../../../node_modules/.prisma/client/index";
-
 
 export async function finalizeTransfer(transferId: string) {
   const claimed = await prisma.transferDraft.updateMany({
-    where: { id: transferId, status: TransferStatus.CREATED, startedAt: null},
-    data: { status: TransferStatus.RUNNING, startedAt: new Date() }
+    where: { id: transferId, status: "CREATED", startedAt: null},
+    data: { status: "RUNNING", startedAt: new Date() }
   })
   if (claimed.count === 0) {
     const t = await prisma.transferDraft.findUnique({ where: { id: transferId }});
     if (!t) throw new Error("Transfer data lost. Please try again")
-    if (t.status == TransferStatus.RUNNING) {
+    if (t.status == "RUNNING") {
       const result = await runTransfer({ id: transferId })
       return result;
     }
 
     else if (
-      t.status == TransferStatus.PARTIAL || 
-      t.status == TransferStatus.FAILED || 
-      t.status == TransferStatus.SUCCESS) {
+      t.status == "PARTIAL" || 
+      t.status == "FAILED" || 
+      t.status == "SUCCESS") {
       return { source: t.source, dest: t.dest, added: t?.added ?? 0, failed: t?.failed ?? 0, copies: t?.copies ?? 0, srcPlaylistName: t?.srcPlaylistName ?? "", destPlaylistName: t?.destPlaylistName ?? "" };
     }
   }
