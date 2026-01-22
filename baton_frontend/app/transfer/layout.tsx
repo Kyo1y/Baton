f2721674cb8a3bdee18fe5474ef4bada38830344
Kairat Sadyrbekov <kyoly@Kairats-MacBook-Air.local>
2026-01-22 17:57:58 -0500

    check session

--- baton_frontend/app/transfer/layout.tsx
+++ baton_frontend/app/transfer/layout.tsx
@@ -6,6 +6,7 @@ import TransferStepper from "@/components/TransferStepper";
 
 export default async function TransferLayout({ children }: { children: ReactNode }) {
   const session = await getServerSession(authOptions);
+  console.log(session);
   if (!session) {
     redirect("/api/auth/signin?callbackUrl=%2Ftransfer");
   }
